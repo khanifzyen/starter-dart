ff76ef4a27c0c64f9c37b2a20a7f3945a9d17a77
Akhmad Khanif Zyen <khanif.zyen@gmail.com>
2024-03-11 22:13:37 +0700

    add appwrite

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:io';
 import 'package:dart_appwrite/dart_appwrite.dart';
 
 // This is your Appwrite function
@@ -6,29 +7,42 @@ import 'package:dart_appwrite/dart_appwrite.dart';
 Future<dynamic> main(final context) async {
 // Why not try the Appwrite SDK?
   //
-  // final client = Client()
-  //    .setEndpoint('https://cloud.appwrite.io/v1')
-  //    .setProject(Platform.environment['APPWRITE_FUNCTION_PROJECT_ID'])
-  //    .setKey(Platform.environment['APPWRITE_API_KEY']);
+  final client = Client()
+      .setEndpoint('https://cloud.appwrite.io/v1')
+      .setProject(Platform.environment['APPWRITE_FUNCTION_PROJECT_ID'])
+      .setKey(Platform.environment['APPWRITE_API_KEY']);
+
+  Databases databases = Databases(client);
+  Future result = databases.listDocuments(
+    databaseId: Platform.environment['DATABASE_ID']!,
+    collectionId: Platform.environment['COLLECTION_CATEGORY_ID']!,
+  );
+  result.then((response) {
+    print(response);
+    context.log(response);
+  }).catchError((error) {
+    print(error.response);
+    context.error(error.response);
+  });
 
   // You can log messages to the console
-  context.log('Hello, Logs!');
+  // context.log('Hello, Logs!');
 
   // If something goes wrong, log an error
-  context.error('Hello, Errors!');
+  // context.error('Hello, Errors!');
 
   // The `req` object contains the request data
-  if (context.req.method == 'GET') {
-    // Send a response with the res object helpers
-    // `res.send()` dispatches a string back to the client
-    return context.res.send('Hello, World!');
-  }
+  // if (context.req.method == 'GET') {
+  // Send a response with the res object helpers
+  // `res.send()` dispatches a string back to the client
+  // return context.res.send('Hello, World!');
+  // }
 
   // `res.json()` is a handy helper for sending JSON
-  return context.res.json({
-    'motto': 'Build like a team of hundreds_',
-    'learn': 'https://appwrite.io/docs',
-    'connect': 'https://appwrite.io/discord',
-    'getInspired': 'https://builtwith.appwrite.io',
-  });
+  // return context.res.json({
+  //   'motto': 'Build like a team of hundreds_',
+  //   'learn': 'https://appwrite.io/docs',
+  //   'connect': 'https://appwrite.io/discord',
+  //   'getInspired': 'https://builtwith.appwrite.io',
+  // });
 }
